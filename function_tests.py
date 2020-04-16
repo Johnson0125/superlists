bcbfd57002983b08f6731170da78a3cfe2e22b45
johnson <zhangsen10@cnhqd.net>
2020-04-16 17:15:36 +0800

    add many test case

--- function_tests.py
+++ function_tests.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 from selenium import webdriver
 
@@ -6,6 +7,8 @@ from selenium import webdriver
 # browser.get('http://localhost:8000')
 #
 # assert 'To-Do' in browser.title, "Browser title was "+ browser.title
+from selenium.webdriver.common.keys import Keys
+
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -18,6 +21,21 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get("http://localhost:8000")
 
         self.assertIn("To-Do", self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name('h1').text()
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder')), 'Enter a to-do item'
+
+        inputbox.send_keys('buy peacock feathers')
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name("tr")
+        self.assertTrue(any(row.text == '1:buy peacock features' for row in rows))
 
         self.fail('finish the test!')
 
