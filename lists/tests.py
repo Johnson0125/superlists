b211200ff265b524c54602772d516cef416d13b0
johnson <zhangsen10@cnhqd.net>
2020-04-16 15:38:29 +0800

    tdd pass

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 
 # Create your tests here.
 class SmokeTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_when_root_url_then_goto_the_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
