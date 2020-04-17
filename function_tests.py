2debbecd88fdcaa334d82a6671d6c44dd3026551
johnson <zhangsen10@cnhqd.net>
2020-04-17 17:32:48 +0800

    fix some ut errors

--- function_tests.py
+++ function_tests.py
@@ -17,6 +17,25 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def test_can_start_a_list_and_retrieve_it_later2(self):
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1:buy peacock feathers')
+
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table("1:buy peacock feathers")
+        self.check_for_row_in_list_table('Use peacock feathers to make a fly')
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://localhost:8000/")
 
